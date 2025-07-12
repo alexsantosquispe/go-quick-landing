da251291b5e182df67478587d65ed09a46317998
Alex Santos <alexsantosquispe@gmail.com>
2025-07-12 13:10:10 -0400

    favicon added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { useEffect } from 'react';
-import { Footer } from './components/molecules/Footer';
-import { Navbar } from './components/molecules/Navbar';
 import { CommentsSection } from './components/organisms/CommentsSection';
+import { Footer } from './components/molecules/Footer';
 import { Hero } from './components/organisms/Hero';
 import { MobileAppSection } from './components/organisms/MobileAppSection';
+import { Navbar } from './components/molecules/Navbar';
 import { OurService } from './components/organisms/OurService';
 import { PopularBrands } from './components/organisms/PopularBrands';
 import { PopularCategories } from './components/organisms/PopularCategories';
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:3000/purchases');
-      const jsonRes = await response.json();
-      console.log(jsonRes);
-    };
-    fetchData();
-  }, []);
-
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Navbar />
